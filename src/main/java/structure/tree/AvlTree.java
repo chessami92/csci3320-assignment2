a6a124be11e2460777503ecbb902a65a45711133
Josh DeWitt <jdewitt@unomaha.edu>
2013-02-17 14:18:06 -0600

    Renamed AvlNodeModifier to AvlTree.

--- src/main/java/structure/tree/AvlTree.java
+++ src/main/java/structure/tree/AvlTree.java
@@ -4,11 +4,11 @@ package structure.tree;
  * Created by: Josh
  * On: 2/16/13 10:55 PM
  */
-public class AvlNodeModifier<T extends Comparable<T>> extends HvlTree {
+public class AvlTree<T extends Comparable<T>> extends HvlTree {
     /*
      * Constructor - Creates an HVL tree with allowed imbalance of 1.
      */
-    AvlNodeModifier() {
+    AvlTree() {
         super(1);
     }
 }
